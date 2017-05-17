3ff29a2db70bf0416e59f8d223b4501c2ef95a9d
daurnimator <quae@daurnimator.com>
2017-05-17 18:43:47 +1000

    src/web-cli-lua.js: Pass code origin; re-throw passing errors; cleanup when done

--- src/web-cli-lua.js
+++ src/web-cli-lua.js
@@ -19,4 +19,13 @@ lua.lua_pop(L, 1); /* remove lib */
 lua.lua_pushstring(L, lua.to_luastring(lua.FENGARI_COPYRIGHT));
 lua.lua_setglobal(L, lua.to_luastring("_COPYRIGHT"));
 
-lauxlib.luaL_dostring(L, lua.to_luastring(document.getElementById("cli.lua").textContent));
+let tag = document.getElementById("cli.lua");
+let code = tag.innerHTML;
+let chunkname = tag.src ? ("@"+tag.src) : tag.id ? ("="+tag.id) : code;
+let ok = lauxlib.luaL_loadbuffer(L, lua.to_luastring(code), null, lua.to_luastring(chunkname));
+if (ok !== lua.LUA_OK) {
+	let msg = lua.lua_tojsstring(L, -1);
+	lua.lua_pop(L, 1);
+	throw Error(msg);
+}
+lua.lua_call(L, 0, 0, 0);
