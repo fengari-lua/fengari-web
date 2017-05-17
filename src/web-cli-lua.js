1044763868bbd1c889971f2a93cb5b44d1a11087
daurnimator <quae@daurnimator.com>
2017-05-18 00:01:41 +1000

    Follow src attribute on script tags with type="text/lua"
    Set async attribute to avoid a synchronous XHR
    
    This avoids having to keep code in web-cli.lua and lua-web-cli.html in sync

--- src/web-cli-lua.js
+++ src/web-cli-lua.js
@@ -19,10 +19,8 @@ lua.lua_pop(L, 1); /* remove lib */
 lua.lua_pushstring(L, lua.to_luastring(lua.FENGARI_COPYRIGHT));
 lua.lua_setglobal(L, lua.to_luastring("_COPYRIGHT"));
 
-const run_lua_script_tag = function(tag) {
-	let code = tag.innerHTML;
-	let chunkname = tag.src ? ("@"+tag.src) : tag.id ? ("="+tag.id) : code;
-	let ok = lauxlib.luaL_loadbuffer(L, lua.to_luastring(code), null, lua.to_luastring(chunkname));
+const run_lua_script = function(code, chunkname) {
+	let ok = lauxlib.luaL_loadbuffer(L, code, null, chunkname);
 	if (ok === lua.LUA_OK) {
 		ok = lua.lua_pcall(L, 0, 0, 0); /* TODO: use message handler to add traceback */
 	}
@@ -30,6 +28,44 @@ const run_lua_script_tag = function(tag) {
 		let msg = lauxlib.luaL_tolstring(L, -1);
 		lua.lua_pop(L, 1);
 		console.error(lua.to_jsstring(msg));
+	}
+};
+
+const handle_xhr_response = function(tag, xhr) {
+	if (xhr.status == 200) {
+		let code = xhr.response;
+		if (typeof code === "string") {
+			code = lua.to_luastring(code);
+		} else { /* ArrayBuffer */
+			code = Array.from(new Uint8Array(code));
+		}
+		let chunkname = "@"+tag.src;
+		run_lua_script(code, lua.to_luastring(chunkname));
+	}
+};
+
+const run_lua_script_tag = function(tag) {
+	if (tag.src) {
+		let xhr = new XMLHttpRequest();
+		/* JS script tags are async after document has loaded */
+		if (document.readyState === "complete" || tag.async) {
+			xhr.responseType = "arraybuffer";
+			xhr.onreadystatechange = function() {
+				if (this.readyState == 4) {
+					handle_xhr_response(tag, this);
+				}
+			};
+			xhr.open("GET", tag.src, true);
+			xhr.send();
+		} else {
+			xhr.open("GET", tag.src, false);
+			xhr.send();
+			handle_xhr_response(tag, xhr);
+		}
+	} else {
+		let code = tag.innerHTML;
+		let chunkname = tag.id ? ("="+tag.id) : code;
+		run_lua_script(lua.to_luastring(code), lua.to_luastring(chunkname));
 	}
 };
 
