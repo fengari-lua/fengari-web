55b16ed77ba5db186a1e14e447170a8e77a232e3
daurnimator <quae@daurnimator.com>
2017-12-28 15:28:31 +1100

    webpack.config.js: Use tabs

--- webpack.config.js
+++ webpack.config.js
@@ -4,35 +4,35 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = [
-    {
-        entry: './src/fengari-web.js',
-        target: 'web',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'fengari-web.js',
-            library: 'fengari',
-            libraryTarget: 'umd'
-        },
-        node: false,
-        module: {
-            rules: [
-                {
-                    test: [/\.js$/],
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [['env', {
-                            "targets": {
-                                "browsers": ["last 2 versions", "safari >= 7"]
-                            }
-                        }]]
-                    }
-                }
-            ]
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                "typeof process": JSON.stringify("undefined")
-            })
-        ]
-    }
+	{
+		entry: './src/fengari-web.js',
+		target: 'web',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'fengari-web.js',
+			library: 'fengari',
+			libraryTarget: 'umd'
+		},
+		node: false,
+		module: {
+			rules: [
+				{
+					test: [/\.js$/],
+					loader: 'babel-loader',
+					options: {
+						presets: [['env', {
+							"targets": {
+								"browsers": ["last 2 versions", "safari >= 7"]
+							}
+						}]]
+					}
+				}
+			]
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				"typeof process": JSON.stringify("undefined")
+			})
+		]
+	}
 ];
