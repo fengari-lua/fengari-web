bc5ae352793df430e1100a77321a6d8ecf3fc38f
daurnimator <quae@daurnimator.com>
2018-01-28 14:20:40 +1100

    src/fengari-web.js: Update to new fengari api

--- src/fengari-web.js
+++ src/fengari-web.js
@@ -1,42 +1,108 @@
 "use strict";
 
-import {lua, lauxlib, lualib} from 'fengari';
+import {
+	FENGARI_AUTHORS,
+	FENGARI_COPYRIGHT,
+	FENGARI_RELEASE,
+	FENGARI_VERSION,
+	FENGARI_VERSION_MAJOR,
+	FENGARI_VERSION_MINOR,
+	FENGARI_VERSION_NUM,
+	FENGARI_VERSION_RELEASE,
+
+	luastring_eq,
+	luastring_indexOf,
+	luastring_of,
+	to_jsstring,
+	to_luastring,
+	to_uristring,
+
+	lua,
+	lauxlib,
+	lualib
+} from 'fengari';
 import * as interop from 'fengari-interop';
 
+const {
+	LUA_ERRRUN,
+	LUA_ERRSYNTAX,
+	LUA_OK,
+	lua_Debug,
+	lua_getinfo,
+	lua_getstack,
+	lua_gettop,
+	lua_insert,
+	lua_pcall,
+	lua_pop,
+	lua_pushcfunction,
+	lua_pushstring,
+	lua_remove,
+	lua_setglobal,
+	lua_tojsstring
+} = lua;
+const {
+	luaL_loadbuffer,
+	luaL_newstate,
+	luaL_requiref
+} = lauxlib;
+const {
+	checkjs,
+	luaopen_js,
+	push,
+	tojs
+} = interop;
+
 export {
+	FENGARI_AUTHORS,
+	FENGARI_COPYRIGHT,
+	FENGARI_RELEASE,
+	FENGARI_VERSION,
+	FENGARI_VERSION_MAJOR,
+	FENGARI_VERSION_MINOR,
+	FENGARI_VERSION_NUM,
+	FENGARI_VERSION_RELEASE,
+
+	luastring_eq,
+	luastring_indexOf,
+	luastring_of,
+	to_jsstring,
+	to_luastring,
+	to_uristring,
+
 	lua,
 	lauxlib,
 	lualib,
 	interop
 };
-export const L = lauxlib.luaL_newstate();
+
+export const L = luaL_newstate();
 
 /* open standard libraries */
 lualib.luaL_openlibs(L);
-lauxlib.luaL_requiref(L, lua.to_luastring("js"), interop.luaopen_js, 1);
-lua.lua_pop(L, 1); /* remove lib */
+luaL_requiref(L, to_luastring("js"), luaopen_js, 1);
+lua_pop(L, 1); /* remove lib */
 
-lua.lua_pushstring(L, lua.to_luastring(lua.FENGARI_COPYRIGHT));
-lua.lua_setglobal(L, lua.to_luastring("_COPYRIGHT"));
+lua_pushstring(L, to_luastring(FENGARI_COPYRIGHT));
+lua_setglobal(L, to_luastring("_COPYRIGHT"));
 
 /* Helper function to load a JS string of Lua source */
 export function load(source, chunkname) {
 	if (typeof source == "string")
-		source = lua.to_luastring(source);
+		source = to_luastring(source);
 	else if (!Array.isArray(source))
 		throw TypeError("expected string or array of bytes");
 
 
-	chunkname = chunkname?lua.to_luastring(chunkname):null;
-	let ok = lauxlib.luaL_loadbuffer(L, source, null, chunkname);
+	chunkname = chunkname?to_luastring(chunkname):null;
+	let ok = luaL_loadbuffer(L, source, null, chunkname);
 	let res;
-	if (ok === lua.LUA_ERRSYNTAX) {
-		res = new SyntaxError(lua.lua_tojsstring(L, -1));
+	if (ok === LUA_ERRSYNTAX) {
+		res = new SyntaxError(lua_tojsstring(L, -1));
 	} else {
-		res = interop.tojs(L, -1);
+		res = tojs(L, -1);
 	}
-	lua.lua_pop(L, 1);
-	if (ok !== lua.LUA_OK) {
+	lua_pop(L, 1);
+	if (ok !== LUA_OK) {
 		throw res;
 	}
 	return res;
@@ -55,25 +121,25 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
 	};
 
 	const msghandler = function(L) {
-		let ar = new lua.lua_Debug();
-		if (lua.lua_getstack(L, 2, ar))
-			lua.lua_getinfo(L, lua.to_luastring("Sl"), ar);
-		interop.push(L, new ErrorEvent("error", {
+		let ar = new lua_Debug();
+		if (lua_getstack(L, 2, ar))
+			lua_getinfo(L, to_luastring("Sl"), ar);
+		push(L, new ErrorEvent("error", {
 			bubbles: true,
 			cancelable: true,
-			message: lua.lua_tojsstring(L, 1),
-			error: interop.tojs(L, 1),
-			filename: ar.short_src ? lua.to_jsstring(ar.short_src) : void 0,
+			message: lua_tojsstring(L, 1),
+			error: tojs(L, 1),
+			filename: ar.short_src ? to_jsstring(ar.short_src) : void 0,
 			lineno: ar.currentline > 0 ? ar.currentline : void 0
 		}));
 		return 1;
 	};
 
 	const run_lua_script = function(tag, code, chunkname) {
-		let ok = lauxlib.luaL_loadbuffer(L, code, null, chunkname);
+		let ok = luaL_loadbuffer(L, code, null, chunkname);
 		let e;
-		if (ok === lua.LUA_ERRSYNTAX) {
-			let msg = lua.lua_tojsstring(L, -1);
+		if (ok === LUA_ERRSYNTAX) {
+			let msg = lua_tojsstring(L, -1);
 			let filename = tag.src?tag.src:document.location;
 			let lineno = void 0; /* TODO: extract out of msg */
 			let syntaxerror = new SyntaxError(msg, filename, lineno);
@@ -83,35 +149,35 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
 				filename: filename,
 				lineno: lineno
 			});
-		} else if (ok === lua.LUA_OK) {
+		} else if (ok === LUA_OK) {
 			/* insert message handler below function */
-			let base = lua.lua_gettop(L);
-			lua.lua_pushcfunction(L, msghandler);
-			lua.lua_insert(L, base);
+			let base = lua_gettop(L);
+			lua_pushcfunction(L, msghandler);
+			lua_insert(L, base);
 			/* set document.currentScript.
 			   We can't set it normally; but we can create a getter for it, then remove the getter */
 			Object.defineProperty(document, 'currentScript', {
 				value: tag,
 				configurable: true
 			});
-			ok = lua.lua_pcall(L, 0, 0, base);
+			ok = lua_pcall(L, 0, 0, base);
 			/* Remove the currentScript getter installed above; this restores normal behaviour */
 			delete document.currentScript;
 			/* Remove message handler */
-			lua.lua_remove(L, base);
+			lua_remove(L, base);
 			/* Check if normal error that msghandler would have handled */
-			if (ok === lua.LUA_ERRRUN) {
-				e = interop.checkjs(L, -1);
+			if (ok === LUA_ERRRUN) {
+				e = checkjs(L, -1);
 			}
 		}
-		if (ok !== lua.LUA_OK) {
+		if (ok !== LUA_OK) {
 			if (e === void 0) {
 				e = new ErrorEvent("error", {
-					message: lua.lua_tojsstring(L, -1),
-					error: interop.tojs(L, -1)
+					message: lua_tojsstring(L, -1),
+					error: tojs(L, -1)
 				});
 			}
-			lua.lua_pop(L, 1);
+			lua_pop(L, 1);
 			if (window.dispatchEvent(e)) {
 				console.error("uncaught exception", e.error);
 			}
@@ -122,7 +188,7 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
 		if (xhr.status >= 200 && xhr.status < 300) {
 			let code = xhr.response;
 			if (typeof code === "string") {
-				code = lua.to_luastring(xhr.response);
+				code = to_luastring(xhr.response);
 			} else { /* is an array buffer */
 				code = new Uint8Array(code);
 			}
@@ -136,7 +202,7 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
 	/* in main browser window */
 	const run_lua_script_tag = function(tag) {
 		if (tag.src) {
-			let chunkname = lua.to_luastring("@"+tag.src);
+			let chunkname = to_luastring("@"+tag.src);
 			/* JS script tags are async after document has loaded */
 			if (document.readyState === "complete" || tag.async) {
 				if (typeof fetch === "function") {
@@ -175,8 +241,8 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
 				process_xhr_response(xhr, tag, chunkname);
 			}
 		} else {
-			let code = lua.to_luastring(tag.innerHTML);
-			let chunkname = tag.id ? lua.to_luastring("="+tag.id) : code;
+			let code = to_luastring(tag.innerHTML);
+			let chunkname = tag.id ? to_luastring("="+tag.id) : code;
 			run_lua_script(tag, code, chunkname);
 		}
 	};
