a07e9e8e5607ad4f4dff543cbb195912505d86c5
daurnimator <quae@daurnimator.com>
2018-03-25 14:28:02 +1100

    webpack.config.js: Add name for script-tag output

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 module.exports = [
 	{
+		/*
+		This target produces a file suitable for embedding onto any webpage via a <script> tag.
+		*/
+		name: 'script-tag',
 		entry: './src/fengari-web.js',
 		target: 'web',
 		output: {
